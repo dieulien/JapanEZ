1cba483c6e55d24f5afa613f5b08be4773147765
tale97 <ttle@college.harvard.edu>
2020-07-13 00:33:19 -0400

    added Register, Signin components, altho no functionality yet

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { Button } from "@material-ui/core";
 
-const NavBar = () => {
-    return(
-        <div>
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="title" color="inherit">
-                    Learn Hiragana Non-stop
-                </Typography>
-            </Toolbar>
-        </AppBar>
-        </div>
-    )
-}
+const NavBar = ({ onRouteChange }) => {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            Learn Hiragana Non-stop
+          </Typography>
+          <Button
+            color="inherit"
+            onClick={() => onRouteChange("signin")}
+            className="f3 link dim black underline pa3 pointer"
+          >
+            Signout
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 export default NavBar;
