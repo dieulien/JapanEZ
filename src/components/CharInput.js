2d29f56edfb65da871a4d2ee13abb6b613ec058b
tale97 <ttle@college.harvard.edu>
2020-07-25 15:56:08 -0400

    card change color as user types, without regard to when user press Space

--- src/components/CharInput.js
+++ src/components/CharInput.js
@@ -2,13 +2,14 @@ import React from "react";
 import "../containers/App.css";
 import { Input } from "@material-ui/core";
 
-const CharInput = ({ onInputChange }) => {
+const CharInput = ({ onInputChange, spacePress }) => {
   return (
     <form>
       <Input
         placeholder="Start typing here..."
         inputProps={{ "aria-label": "description" }}
         onChange={onInputChange}
+        onKeyPress={spacePress}
       />
     </form>
   );
