3be15183ac36fd059f89601b62eb80aca28b9d5a
tale97 <ttle@college.harvard.edu>
2020-07-26 23:20:32 -0400

    user input with no space: verify if what user type is correct/incorrect by highlighting card as green/red

--- src/components/CharList.js
+++ src/components/CharList.js
@@ -2,37 +2,56 @@ import React from "react";
 import Char from "./Char.js";
 import { Grid } from "@material-ui/core";
 
-const decideCardState = (userInput, cardIndex, currentChar) => {
-  const userInputChar = userInput
-    .split(" ")
-    .filter((element) => element.length != 0);
-  const userInputLength = userInputChar.length;
-  console.log("DEBUG", userInputChar);
-  console.log("current Char", currentChar);
-  if (userInputLength >= cardIndex + 1) {
-    if (userInputChar[cardIndex] === currentChar) {
-      return "correct";
-    } else {
-      return "incorrect";
-    }
-  } else {
-    return "";
-  }
-};
-
 class CharList extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  partitionCharIndex = (charList) => {
+    var characterCounter = 0;
+
+    const indexPartition = charList.map(
+      (item) => (characterCounter += item.length)
+    );
+
+    return indexPartition;
+  };
+
+  decideCardState = (userInput, currentChar, i, indexPartition) => {
+    var userChar = "";
+    if (userInput.length >= indexPartition[i]) {
+      if (i === 0) {
+        userChar = userInput.slice(0, indexPartition[i]);
+      } else {
+        userChar = userInput.slice(indexPartition[i - 1], indexPartition[i]);
+      }
+      if (userChar === currentChar) {
+        return "correct";
+      } else {
+        return "incorrect";
+      }
+    } else {
+      return "";
+    }
+  };
+
   render() {
+    const charList = this.props.charsToRead.map((item) => item.romaji);
+    const phrase = charList.reduce((acc, item) => acc.concat(item), "");
+    const indexPartition = this.partitionCharIndex(charList);
+
     const charsArrayDisplay = this.props.charsToRead.map((item, i) => {
       return (
         <Grid item key={i}>
           <Char
             char={item.char}
             key={i}
-            cardState={decideCardState(this.props.userInput, i, item.romaji)}
+            cardState={this.decideCardState(
+              this.props.userInput,
+              item.romaji,
+              i,
+              indexPartition
+            )}
           />
         </Grid>
       );
