0f24b3e9edeb5caa7f7f4c5990df1211175a5877
tale97 <ttle@college.harvard.edu>
2020-07-26 23:20:32 -0400

    user input with no space: verify if what user type is correct/incorrect by highlighting card as green/red

--- src/components/Char.js
+++ src/components/Char.js
@@ -2,13 +2,31 @@ import React from "react";
 import "../containers/App.css";
 import "./Char.css";
 
-const Char = (props) => {
-  const className = `tmw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10 ${props.cardState}`;
-  return (
-    <div className={className}>
-      <h1 className="char">{props.char}</h1>
-    </div>
-  );
-};
+class Char extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardStateDecided: false,
+      cardState: "em",
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("COMPONENT UPDATE 1", prevProps);
+    console.log("COMPONENT UPDATE 2", this.props);
+    if (this.props.cardState !== prevProps.cardState) {
+      this.setState({ cardState: this.props.cardState });
+    }
+  }
+
+  render() {
+    const className = `tmw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10 ${this.state.cardState}`;
+    return (
+      <div className={className}>
+        <h1 className="char">{this.props.char}</h1>
+      </div>
+    );
+  }
+}
 
 export default Char;
