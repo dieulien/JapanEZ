194d84af1f46fb4991b078d8d8180cb758fa1a0f
dieulien <dieulien.tran181@gmail.com>
2020-07-30 18:52:06 +0700

    added some flexibility which server to run

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,17 @@
 export const USER_INPUT = "USER_INPUT";
 
-const local = false;
+const method = "tuanHeroku";
+
 var REGISTER_URL = "https://localhost:3000/register";
 var SIGNIN_URL = "https://localhost:3000/signin";
 var PROFILE_URL = "https://localhost:3000/profile/";
 
-if (!local){
-    REGISTER_URL = "https://shrouded-harbor-11572.herokuapp.com/register";
-    SIGNIN_URL = "https://shrouded-harbor-11572.herokuapp.com/signin";
-    PROFILE_URL = "https://shrouded-harbor-11572.herokuapp.com/profile/";
-};
+switch(method){
+    case "tuanHeroku":
+        REGISTER_URL = "https://shrouded-harbor-11572.herokuapp.com/register";
+        SIGNIN_URL = "https://shrouded-harbor-11572.herokuapp.com/signin";
+        PROFILE_URL = "https://shrouded-harbor-11572.herokuapp.com/profile/";
+        break;
+}
 
  export {REGISTER_URL, SIGNIN_URL, PROFILE_URL};
