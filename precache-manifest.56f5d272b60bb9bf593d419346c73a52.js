d652b9ae845d752046e947dfbac9cf87bb6cbf72
dieulien <dieulien.tran181@gmail.com>
2020-08-11 19:51:45 +0700

    Updates

---
+++ precache-manifest.56f5d272b60bb9bf593d419346c73a52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57d381d7b7a31e5ba32bc2fa149c483",
+    "url": "/japanese-reading/index.html"
+  },
+  {
+    "revision": "892c6d0c11e62a91ca67",
+    "url": "/japanese-reading/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1be7526b022a63d54c7e",
+    "url": "/japanese-reading/static/css/main.fa1da032.chunk.css"
+  },
+  {
+    "revision": "892c6d0c11e62a91ca67",
+    "url": "/japanese-reading/static/js/2.4ffe3982.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/japanese-reading/static/js/2.4ffe3982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be7526b022a63d54c7e",
+    "url": "/japanese-reading/static/js/main.46806477.chunk.js"
+  },
+  {
+    "revision": "e302560d5cd9bb944f5b",
+    "url": "/japanese-reading/static/js/runtime-main.ba7e5498.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/japanese-reading/precache-manifest.50aedbd6effd8b6e67718ac470706a91.js"
+  "/japanese-reading/precache-manifest.56f5d272b60bb9bf593d419346c73a52.js"
 );
 
 self.addEventListener('message', (event) => {
