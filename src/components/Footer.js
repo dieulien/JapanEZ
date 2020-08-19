288ad52c6e656d673f0c4e5379acb4827247f5d3
tale97 <ttle@college.harvard.edu>
2020-08-19 10:28:08 -0400

    fix issue with vertical scrollbar on chrome keep disappearing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,22 @@ import { TOFUGU_LINK, WORD_LINK } from "../constants";
 const Footer = () => {
   return (
     <div className="main-footer">
-      <div className="container">
+      <div>
+        <p>
+          Mnemonics taken from <a href={TOFUGU_LINK}>tofugu.com</a>
+        </p>
+        <p>
+          Japanese words taken from{" "}
+          <a href={WORD_LINK}>reddit.com/r/LearnJapanese</a>
+        </p>
+      </div>
+      <hr />
+      <div>
+        <small>Copyright © 2020 Tuan Le, Lien Tran. All Rights Reserved</small>
+      </div>
+
+      {/* uncomment below for 2nd layout */}
+      {/* <div className="container">
         <div className="row">
           <div className="col">
             <ul className="list-unstyled">
@@ -24,21 +39,6 @@ const Footer = () => {
             </small>
           </div>
         </div>
-      </div>
-
-      {/* uncomment below for 2nd layout */}
-      {/* <div>
-        <p>
-          Mnemonics taken from <a href={TOFUGU_LINK}>tofugu.com</a>
-        </p>
-        <p>
-          Japanese words taken from{" "}
-          <a href={WORD_LINK}>reddit.com/r/LearnJapanese</a>
-        </p>
-      </div>
-      <hr />
-      <div>
-        <small>Copyright © 2020 Tuan Le, Lien Tran. All Rights Reserved</small>
       </div> */}
     </div>
   );
