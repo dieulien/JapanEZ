46b261f9cec75818d360404c4867e205aadeb794
tale97 <ttle@college.harvard.edu>
2020-10-28 11:57:06 -0400

    attempt to fix css gradient on safari

--- src/components/SmallChar.js
+++ src/components/SmallChar.js
@@ -16,14 +16,24 @@ const useStyles = makeStyles((theme) => ({
   cardBG: {
     background: (props) => {
       return props.correctPercent === 0 && props.hintedPercent === 0
+      ? "#d6d6d6"
+      : `-moz-linear-gradient(0deg, rbga(0,128,0,1) 0% ${props.correctPercent}%, rbga(242,181,12,1) ${props.correctPercent}% ${props.hintedPercent}%)`;
+    },
+    background: (props) => {
+      return props.correctPercent === 0 && props.hintedPercent === 0
+      ? "#d6d6d6"
+      : `-webkit-linear-gradient(0deg, rbga(0,128,0,1) 0% ${props.correctPercent}%, rbga(242,181,12,1) ${props.correctPercent}% ${props.hintedPercent}%)`;
+    },
+    background: (props) => {
+      return props.correctPercent === 0 && props.hintedPercent === 0
         ? "#d6d6d6"
         : `linear-gradient(0deg, rbga(0,128,0,1) 0% ${props.correctPercent}%, rbga(242,181,12,1) ${props.correctPercent}% ${props.hintedPercent}%)`;
     },
-    backgroundImage: (props) => {
-      return props.correctPercent === 0 && props.hintedPercent === 0
-        ? "#d6d6d6"
-        : `-webkit-linear-gradient(0deg, rbga(0,128,0,1) 0% ${props.correctPercent}%, rbga(242,181,12,1) ${props.correctPercent}% ${props.hintedPercent}%)`;
-    },
+    // backgroundImage: (props) => {
+    //   return props.correctPercent === 0 && props.hintedPercent === 0
+    //     ? "#d6d6d6"
+    //     : `-webkit-linear-gradient(0deg, rbga(0,128,0,1) 0% ${props.correctPercent}%, rbga(242,181,12,1) ${props.correctPercent}% ${props.hintedPercent}%)`;
+    // },
   },
 }));
 
